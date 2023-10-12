c19b4197eed0488ddfd57d5d461aefe31e046010
likelionbootcamp3 <likelionbootcamp4@gmail.com>
2023-10-12 17:14:59 +0700

    Update chat.tsx

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,10 +1,12 @@
+import { deepStrictEqual } from "assert";
+import { findSourceMap } from "module";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Chat() {
   return (
     <div className="fixed bottom-4 right-4 z-50 sm:bottom-12 sm:right-12">
-      <Link href="/">
+      <Link href="http://pf.kakao.com/_GjbjG" target="_blank">
         <Image src="/chat.gif" alt="Chat" width={80} height={80} />
       </Link>
     </div>
