22ac1a892a7616d6082d35ebeaa7f33393ee3df0
likelionbootcamp3 <likelionbootcamp4@gmail.com>
2023-10-12 10:49:37 +0700

    Update sticky-button.tsx

--- src/components/sticky-button.tsx
+++ src/components/sticky-button.tsx
@@ -3,7 +3,7 @@ import { Button } from "./ui/button";
 
 export default function StickyButton() {
   return (
-    <div className="fixed left-1/2 bottom-4 -translate-x-1/2 max-w-xs w-full z-50">
+    <div className="fixed left-1/2 bottom-4 -translate-x-1/2 max-w-[300px] sm:max-w-xs w-full z-50 px-8">
       <Button size="lg" className="w-full" asChild>
         <Link href="/apply">Tham gia ngay</Link>
       </Button>
